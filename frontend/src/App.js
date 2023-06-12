6285f71e3f6ff4d0d70478178cff93dad7f35e92
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-06-11 20:48:51 -0400

    Added /team to routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Schedule } from './pages/Schedule';
-import { NotFound } from './pages/NotFound';
-import { Home } from './pages/Home';
-import { NavBar } from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Schedule } from "./pages/Schedule";
+import { NotFound } from "./pages/NotFound";
+import { Home } from "./pages/Home";
+import Team from "./pages/Team";
+import { NavBar } from "./components/NavBar";
 
 function App() {
-
-
   return (
-      <Router>
-      <NavBar/>
-        <head>
-          <link href='https://fonts.googleapis.com/css?family=Outfit' rel='stylesheet'></link>
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-          <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"></link>
-        </head>
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/home" element={<Home/>} />
-            <Route path="/schedule" element={<Schedule/>}/>
-            <Route path="*" element={<NotFound/>}/>
-          </Routes>
-      </Router>
+    <Router>
+      <NavBar />
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Outfit"
+          rel="stylesheet"
+        ></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+          rel="stylesheet"
+        ></link>
+      </head>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
