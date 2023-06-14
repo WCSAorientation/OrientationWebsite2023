9ecd0bb130d7c50b2689c7fb8d38ffbfc1f73a2b
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-06-14 12:53:12 -0400

    fixed font size

--- frontend/src/components/TeamTile.js
+++ frontend/src/components/TeamTile.js
@@ -4,7 +4,7 @@ function TeamTile({ name, position, about, imageName }) {
   let displayName = name.split(" ").map((namePart, index) => (
     <span
       key={index}
-      className="first-letter:text-3xl inline-block whitespace-break-spaces"
+      className="first-letter:text-4xl inline-block whitespace-break-spaces"
     >
       {index !== 0 && " "}
       {namePart.toUpperCase()}
@@ -12,7 +12,7 @@ function TeamTile({ name, position, about, imageName }) {
   ));
 
   let aboutDisplay = (
-    <ul className="list-disc font-medium ml-6 mt-2">
+    <ul className="list-disc text-lg font-medium ml-6 mt-2">
       {about.map((point, pointNumber) => (
         <li key={pointNumber}>{point}</li>
       ))}
@@ -25,10 +25,10 @@ function TeamTile({ name, position, about, imageName }) {
         <img src={"./person-background.svg"}></img>
         <img src={imageName} className="absolute left-0 bottom-0 max-h-full" />
         <div className="absolute top-0 left-[34%] right-[10%] p-3 pl-100">
-          <h2 className="font-[700] text-2xl text-[#000000] -mb-1.5">
+          <h2 className="font-[700] text-3xl text-[#000000] -mb-1.5">
             {displayName}
           </h2>
-          <span className="text-[#628080]">{position}</span>
+          <span className="text-[#628080] text-lg">{position}</span>
           {aboutDisplay}
         </div>
       </div>
