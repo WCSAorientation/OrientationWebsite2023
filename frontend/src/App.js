9b643ffd59636fd3fa88997bbd3ce7221112caec
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-06-14 09:35:56 -0400

    imported all Roboto font weights

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
           rel="stylesheet"
         ></link>
       </head>
