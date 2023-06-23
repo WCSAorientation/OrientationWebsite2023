76c81aa4ecb11083f93088cc52bfbcb7063dae82
SeongminChang <seongmin.chang@mail.utoronto.ca>
2023-06-22 20:03:24 -0400

    bruteforced faq page, populate method soon

---
+++ frontend/src/pages/FAQ.js
@@ -0,0 +1,12 @@
+import { NavBar } from "../components/NavBar";
+import faqQuestion from "../data/faq-questions.json";
+
+function faq() {
+    return (
+      <>
+        <body className="bg-[#182E2E] min-h-screen">
+        <NavBar isDark={true} />
+      </body>
+    </>
+  );
+} 
